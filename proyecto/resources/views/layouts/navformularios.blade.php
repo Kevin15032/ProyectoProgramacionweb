22f12198b370bfb922bc5f264737d822b4cbbe71
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2024-11-05 14:44:43 -0600

    update navformularios

--- proyecto/resources/views/layouts/navformularios.blade.php
+++ proyecto/resources/views/layouts/navformularios.blade.php
@@ -16,7 +16,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white shadow mb-4">
         <div class="container">
-            <a class="navbar-brand font-weight-bold text-dark" href="{{ route('rutaInicio') }}">Panel de Administración</a>
+            <a class="navbar-brand font-weight-bold text-dark" href="{{ route('rutaInicio') }}">Panel de Formularios</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
